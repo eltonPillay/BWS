3729075a71d62be681090b50e714d11d2f55e92b
eltonPillay <biduudywebsolutions@gmail.com>
2023-05-05 22:23:46 +0200

    added navbar - happy with transparent back

--- apps.js
+++ apps.js
@@ -1,1 +1,18 @@
+/* navbar - change black when scrolling down */
 
+
+var nav = document.querySelector('nav');
+var logo = document.getElementById('imgl');
+
+        window.addEventListener('scroll', function () {
+            if (window.pageYOffset > 100) {
+                nav.classList.add('bg-primary', 'shadow');
+                nav.classList.add('navbar-dark');
+                logo.src = "/img/Logo/2.png";
+            } else {
+                nav.classList.remove('bg-primary', 'shadow');
+                nav.classList.remove('navbar-dark');
+                nav.classList.add('navbar-light');
+                logo.src = "/img/Logo/1.png";
+            }
+        });
