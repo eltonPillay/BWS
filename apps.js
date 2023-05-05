96cb3a55fe80735fc99669dbce51d0af5569c746
eltonPillay <biduudywebsolutions@gmail.com>
2023-05-05 22:32:05 +0200

    improved my JS code - more streamlined, no changes to functionality

--- apps.js
+++ apps.js
@@ -1,18 +1,18 @@
-/* navbar - change black when scrolling down */
+/* navbar - change to blue when scrolling down */
 
 
 const nav = document.querySelector('nav');
 const logo = document.getElementById('imgl');
+const scrollThreshold = 100;
 
-        window.addEventListener('scroll', function () {
-            if (window.pageYOffset > 100) {
-                nav.classList.add('bg-primary', 'shadow');
-                nav.classList.add('navbar-dark');
-                logo.src = "/img/Logo/2.png";
-            } else {
-                nav.classList.remove('bg-primary', 'shadow');
-                nav.classList.remove('navbar-dark');
-                nav.classList.add('navbar-light');
-                logo.src = "/img/Logo/1.png";
-            }
-        });
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > scrollThreshold) {
+        nav.classList.add('bg-primary', 'shadow');
+        nav.classList.replace('navbar-light', 'navbar-dark');
+        logo.src = "/img/Logo/2.png";
+    } else {
+        nav.classList.remove('bg-primary', 'shadow');
+        nav.classList.replace('navbar-dark', 'navbar-light');
+        logo.src = "/img/Logo/1.png";
+    }
+});
