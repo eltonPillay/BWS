3e9c5bb758a6bf4e76d13496166ea8e320b1e084
eltonPillay <biduudywebsolutions@gmail.com>
2023-05-07 13:30:49 +0200

    created services section and optimised navbar to color scheme

--- apps.js
+++ apps.js
@@ -7,12 +7,8 @@ const scrollThreshold = 100;
 
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > scrollThreshold) {
-        nav.classList.add('bg-primary', 'shadow');
-        nav.classList.replace('navbar-light', 'navbar-dark');
-        logo.src = "/img/Logo/2.png";
+        nav.classList.add('navcol', 'shadow');
     } else {
-        nav.classList.remove('bg-primary', 'shadow');
-        nav.classList.replace('navbar-dark', 'navbar-light');
-        logo.src = "/img/Logo/1.png";
+        nav.classList.remove('navcol', 'shadow');
     }
 });
