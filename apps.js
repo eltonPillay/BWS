6fcef7163f20e008a2b1689031c12207b402d350
eltonPillay <biduudywebsolutions@gmail.com>
2023-05-08 15:50:16 +0200

    noe major changes

--- apps.js
+++ apps.js
@@ -8,7 +8,9 @@ const scrollThreshold = 100;
 window.addEventListener('scroll', function () {
     if (window.pageYOffset > scrollThreshold) {
         nav.classList.add('navcol', 'shadow');
+        nav.classList.remove('navcol1');
     } else {
         nav.classList.remove('navcol', 'shadow');
+        nav.classList.add('navcol1')
     }
 });
